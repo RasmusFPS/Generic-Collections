c75fe0b6086049a656fd688701d0530f9ffeee95
Rasmus <Rasmus.andersen05@gmail.com>
2025-11-12 22:33:06 +0100

    Added: New files for the program

---
+++ Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collections
+{
+    internal class Restaurant
+    {
+        private Queue<int> _queue;
+        public void AddtoMenu(MenuItem menuItem)
+        {
+            
+        }
+    }
+}
